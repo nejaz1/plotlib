8e427d879452b6418763ab89a39bfbe9a5d1700f
Naveed Ejaz <ejaz.naveed@gmail.com>
2017-03-10 13:34:11 -0500

    minor bug fixs
    1) style names and files were not pointing in the right directory

--- +style/use.m
+++ +style/use.m
@@ -5,17 +5,21 @@ function varargout = use(sty_name)
 % Author
 %   Naveed Ejaz (ejaz.naveed@gmail.com)
 
+%% 0. House-keeping to make sure file and names point to the right dir
+%   - Get the name/location of current plotting style
+sFile   = getenv('PLTSTYLEFILE');
+sName   = getenv('PLTSTYLENAME');
+
+%   - if no environment variable set, use the default/name location
+if isempty(sName)
+    sName   = '$DEFAULT$';
+end;
+if isempty(sFile)
+    sFile   = '$DEFAULT$';
+end;
+
+
 if nargin==0
-    %   - Get the name/location of current plotting style
-    sFile   = getenv('PLTSTYLEFILE');
-    sName   = getenv('PLTSTYLENAME');
-
-    if (isempty(sName) || isempty(sFile))
-        % if no environment variable set, use the default/name location
-        sName   = '$DEFAULT$';
-        sFile   = '$DEFAULT$';
-    end;
-
     if nargout>0
         varargout = {sName,sFile};
     else
